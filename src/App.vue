deb1cd20bfd84f8f23246f04d5cbd1d0d9288a86
shyptia <shyptianastya27@gmail.com>
2024-04-17 20:06:52 +0300

    render blocks and add ability to resize them

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
-import TransactionList from './views/TransactionsList.vue'
+  import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <TransactionList />
+  <header class="header">
+    <nav class="nav">
+      <RouterLink to="/"  :class="{ 'nav-link': true, 'nav-link-active': $route.path === '/' }">Dashboard</RouterLink>
+      <RouterLink to="/transactions" :class="{ 'nav-link': true, 'nav-link-active': $route.path === '/transactions' }">Transactions</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
 </template>
 
 <style scoped>
+.header {
+  background-color: #f2f2f2;
+  padding: 0 40px;
+}
+
+.nav {
+  display: flex;
+}
+
+.nav-link {
+  color: #333;
+  text-decoration: none;
+  padding: 10px 20px;
+  transition: color 0.3s;
+  font-size: 20px;
+  font-weight: 600;
+}
+
+.nav-link-active {
+  background-color: #d0cdcd;
+}
+
+.nav-link:hover {
+  color: #2c2a2a;
+}
 </style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,93 @@
 <template>
-  <div>
-    <h2>Interactive Dashboard</h2>
+  <div class="container">
+    <div
+      v-for="(block, index) in blocks"
+      :key="block.id"
+      class="block"
+      :style="{ width: block.width + 'px', height: block.height + 'px' }"
+      @mousedown="startResize($event, block.id)"
+      @mouseup="endResize"
+      @mousemove="resizeBlock($event, block.id)"
+    >
+      Title {{ index + 1 }}
+    </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const blocks = ref(getBlocksFromLocalStorage());
+
+function getBlocksFromLocalStorage() {
+  const storedBlocks = localStorage.getItem('blocks');
+  if (storedBlocks) {
+    return JSON.parse(storedBlocks);
+  } else {
+    return [
+      { id: 'title1', width: 300, height: 100 },
+      { id: 'title2', width: 300, height: 100 },
+      { id: 'title3', width: 300, height: 100 },
+      { id: 'title4', width: 300, height: 100 },
+      { id: 'title5', width: 300, height: 100 },
+    ];
+  }
+}
+
+let startX, startY;
+let startWidth, startHeight;
+let isResizing = false;
+let activeBlockId = null;
+
+const resizeBlock = (event, id) => {
+  if (!isResizing || id !== activeBlockId) return;
+
+  const offsetX = event.clientX - startX;
+  const offsetY = event.clientY - startY;
+
+  const index = blocks.value.findIndex(block => block.id === id);
+  if (index !== -1) {
+    blocks.value[index].width = startWidth + offsetX;
+    blocks.value[index].height = startHeight + offsetY;
+  }
+};
+
+const startResize = (event, id) => {
+  if (event.target === event.currentTarget) {
+    isResizing = true;
+    startX = event.clientX;
+    startY = event.clientY;
+    const index = blocks.value.findIndex(block => block.id === id);
+    if (index !== -1) {
+      startWidth = blocks.value[index].width;
+      startHeight = blocks.value[index].height;
+    }
+    activeBlockId = id;
+  }
+};
+
+const endResize = () => {
+  isResizing = false;
+  activeBlockId = null;
+  saveBlocksToLocalStorage();
+};
+
+function saveBlocksToLocalStorage() {
+  localStorage.setItem('blocks', JSON.stringify(blocks.value));
+}
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.block {
+  border: 1px solid #000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+}
 </style>
